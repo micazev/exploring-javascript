3066436b5e8e6f8c523f7a948179286562b19328
micazev <micneaz@gmail.com>
2020-06-29 15:15:11 -0300

    fim do mòdulo 7 - vue origamid

---
+++ vuejs/origamid-techno-cursos/src/views/Contato.vue
@@ -0,0 +1,36 @@
+<template>
+  <transition>
+    <div>
+      <h1>Contato</h1>
+      <div>
+        <p>{{api.descricao}}</p>
+        <p>
+          <b>Telefone:</b>
+          {{api.contato.telefone}}
+        </p>
+        <p>
+          <b>Emai:</b>
+          {{api.contato.email}}
+        </p>
+        <p>
+          <b>Endereço:</b>
+          {{api.contato.endereco}}
+        </p>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+import fetchData from "@/mixins/fetchData.js";
+export default {
+  name: "Contato",
+  mixins: [fetchData],
+  created() {
+    this.fetchData("contato");
+  }
+};
+</script>
+
+<style>
+</style>
